7b110e41cc3634dca56d3fdc07688df99e8a2cf8
IAmBornedHope <realbeloff@gmail.com>
2026-05-21 23:38:53 +0300

    Folders and basic cmake

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "libs/Lab_3/array_sequence/ArraySequence.hpp"
+#include "libs/Lab_3/array_sequence/MutableArraySequence.hpp"
+#include "libs/Lab_3/gui/app.hpp"
+#include <iostream>
+
+int main() {
+    
+}
