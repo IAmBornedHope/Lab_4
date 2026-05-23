7f86317409f567764e5bf97d5fd43d784369f90e
IAmBornedHope <realbeloff@gmail.com>
2026-05-23 22:13:15 +0300

    Filter Generator

---
+++ generators/FilterGenerator.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <functional>
+#include <memory>
+#include "IGenerator.hpp"
+
+template<typename T>
+class FilterGenerator : public IGenerator<T> {
+private:
+    std::shared_ptr<IGenerator<T>> base_;
+    std::function<bool(T)> predicate_;
+
+public:
+    FilterGenerator(std::shared_ptr<IGenerator<T>> base, std::function<bool(T)> predicate)
+        : base_{base}, predicate_{predicate} {}
+
+    bool has_next() const override {
+        return base_->has_next();
+    }
+
+    T get_next() override {
+        T value;
+        while (true) {
+            if (!base_->has_next()) {
+                throw IndexOutOfRangeException("FilterGenerator: Поток пуст.");
+            }
+            value = base_->get_next();
+            if (predicate_(value)) {
+                return value;
+            }
+        }
+    }
+
+    Cardinal size() const override {
+        return Cardinal::aleph_null();
+    }
+    
+
+};
---
+++ tests/test_FilterGenerator.cpp
@@ -0,0 +1,47 @@
+#include <gtest/gtest.h>
+#include "../generators/FilterGenerator.hpp"
+#include "../generators/ContainerGenerator.hpp"
+#include "../generators/RecurrentGenerator.hpp"
+#include "../libs/Lab_3/exceptions/Option.hpp"
+#include "../libs/Lab_3/array_sequence/MutableArraySequence.hpp"
+
+TEST(filter_generator, even_test) {
+
+    auto predicate = [](int x) -> bool {
+        return x % 2 == 0;
+    };
+
+    int array[] = {10, 15, 20, 25, 30, 35};
+    MutableArraySequence<int> sequence(array, 6);
+    auto container = std::make_shared<ContainerGenerator<int, MutableArraySequence>>(sequence);
+    auto filter_generator = std::make_shared<FilterGenerator<int>>(container, predicate);
+
+    EXPECT_EQ(filter_generator->size().is_infinite(), 1);
+    EXPECT_EQ(filter_generator->get_next(), 10);
+    EXPECT_EQ(filter_generator->get_next(), 20);
+    EXPECT_EQ(filter_generator->get_next(), 30);
+}
+
+TEST(filter_generator, fibonacci_even_test) {
+    auto fibonacci = [](const MutableArraySequence<int>& sequence) -> int {
+        size_t n = sequence.get_length();
+        if (n < 2) {
+            return 1;
+        }
+        return sequence[n - 1] + sequence[n - 2];
+    };
+
+    auto predicate = [](int x) -> bool {
+        return x % 2 == 0;
+    };
+
+    MutableArraySequence<int> cache;
+    auto container = std::make_shared<RecurrentGenerator<int, MutableArraySequence>>(fibonacci, cache);
+    auto filter_generator = std::make_shared<FilterGenerator<int>>(container, predicate);
+
+    EXPECT_EQ(filter_generator->size().is_infinite(), 1);
+    EXPECT_EQ(filter_generator->get_next(), 2);
+    EXPECT_EQ(filter_generator->get_next(), 8);
+    EXPECT_EQ(filter_generator->get_next(), 34);
+    EXPECT_EQ(filter_generator->get_next(), 144);
+}
