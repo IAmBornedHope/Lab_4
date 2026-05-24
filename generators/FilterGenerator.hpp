1151e5282ccac3572dfd4baa4b5b2ec000c1d146
IAmBornedHope <realbeloff@gmail.com>
2026-05-24 17:22:28 +0300

    Filter fix

--- generators/FilterGenerator.hpp
+++ generators/FilterGenerator.hpp
@@ -8,26 +8,40 @@ class FilterGenerator : public IGenerator<T> {
 private:
     std::shared_ptr<IGenerator<T>> base_;
     std::function<bool(T)> predicate_;
+    mutable bool is_found_ = false;;
+    mutable T filtered_value_;
+    
+    void check_found() const {
+        if (is_found_) {
+            return;
+        }
+
+        while (base_->has_next()) {
+            T value = base_->get_next();
+            if (predicate_(value)) {
+                is_found_ = true;
+                filtered_value_ = value;
+                return;
+            }
+        }
+    }
 
 public:
     FilterGenerator(std::shared_ptr<IGenerator<T>> base, std::function<bool(T)> predicate)
         : base_{base}, predicate_{predicate} {}
 
     bool has_next() const override {
-        return base_->has_next();
+        check_found();
+        return is_found_;
     }
 
     T get_next() override {
-        T value;
-        while (true) {
-            if (!base_->has_next()) {
-                throw IndexOutOfRangeException("FilterGenerator: Поток пуст.");
-            }
-            value = base_->get_next();
-            if (predicate_(value)) {
-                return value;
-            }
+        check_found();
+        if (!is_found_) {
+            throw IndexOutOfRangeException("Читать нечего");
         }
+        is_found_ = false;
+        return filtered_value_;
     }
 
     Cardinal size() const override {
@@ -35,7 +49,14 @@ public:
         if (base_size.is_infinite()) {
             return Cardinal::aleph_null();
         }
-        return base_size;
+
+        auto temp = this->clone();
+        size_t count = 0;
+        while (temp->has_next()) {
+            temp->get_next();
+            ++count;
+        }
+        return Cardinal(count);
     }
 
     std::shared_ptr<IGenerator<T>> clone() const override {
