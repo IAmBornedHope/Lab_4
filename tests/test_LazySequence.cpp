c02bb1a8ff87375ae4bd12d7c000ca968414ba92
IAmBornedHope <realbeloff@gmail.com>
2026-05-25 07:46:28 +0300

    LazyMap tests

--- tests/test_LazySequence.cpp
+++ tests/test_LazySequence.cpp
@@ -2,6 +2,18 @@
 #include "../lazy_sequence/LazySequence.hpp"
 #include "../generators/RecurrentGenerator.hpp"
 
+auto counter = [](const MutableArraySequence<int>& sequence) -> int {
+    if (sequence.get_length() == 0) {
+        return 1;
+    }
+    return sequence.get_last() + 1;
+};
+
+auto function = [](int x) {
+    return x * 2;
+};
+
+
 using LazyInt = LazySequence<int, MutableArraySequence>;
 using GenInt = std::shared_ptr<IGenerator<int>>;
 
@@ -45,4 +57,47 @@ TEST(lazy_sequence_constructor, fibonacci_test) {
     EXPECT_EQ(sequence->get_materialized_count(), 5);
 
     EXPECT_THROW(sequence->get_last(), IndexOutOfRangeException);
+}
+
+
+
+TEST(lazy_operations_test, finite_map_test) {
+    auto function = [](int x) {
+        return x * 2;
+    };
+
+    int array[] = {10, 20, 30};
+    auto sequence = std::make_shared<LazyInt>(array, 3);
+
+    auto mapped = sequence->map<int>(function);
+
+    ASSERT_EQ(sequence->get_materialized_count(), 0);
+    ASSERT_EQ(mapped->get_materialized_count(), 0);
+
+    EXPECT_EQ(mapped->get(1), 40);
+    EXPECT_EQ(mapped->get_materialized_count(), 2);
+    EXPECT_EQ(sequence->get_materialized_count(), 0);
+
+}
+
+TEST(lazy_operations_test, infinite_map_test) {
+
+
+    MutableArraySequence<int> cache;
+    cache.append(1);
+
+    auto generator = std::make_shared<RecurrentGenerator<int, MutableArraySequence>>(counter, cache);
+    auto sequence = std::make_shared<LazyInt>(cache, generator);
+    auto mapped = sequence->map<int>(function);
+
+    ASSERT_EQ(sequence->get_length().is_infinite(), 1);
+    EXPECT_EQ(sequence->get_materialized_count(), 1);
+    
+    EXPECT_EQ(mapped->get_length().is_infinite(), 1);
+    EXPECT_EQ(mapped->get_materialized_count(), 0);
+
+    EXPECT_EQ(mapped->get(Cardinal(2)), 6);
+    EXPECT_EQ(mapped->get_materialized_count(), 3);
+    EXPECT_EQ(sequence->get_materialized_count(), 1);
+
 }
