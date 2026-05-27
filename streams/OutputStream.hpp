2991e45f8624c0fde73c6fb02166794422c62724
IAmBornedHope <realbeloff@gmail.com>
2026-05-28 00:58:47 +0300

    Serializers

--- streams/OutputStream.hpp
+++ streams/OutputStream.hpp
@@ -5,6 +5,27 @@
 template<typename T>
 using Serializer = std::function<std::string(const T&)>;
 
+class Serializers {
+public:
+    static Serializer<int> int_serializer() {
+        return [](int value) {
+            return std::to_string(value);
+        };
+    }
+    
+    static Serializer<double> double_serializer() {
+        return [](double value) {
+            return std::to_string(value);
+        };
+    }
+
+    static Serializer<std::string> string_serializer() {
+        return [](const std::string& value) {
+            return value;
+        };
+    }
+};
+
 template<typename T>
 class OutputStream {
 public:
