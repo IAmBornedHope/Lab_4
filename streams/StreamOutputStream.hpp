51b1a41bed849400226f834bd0dcab3643b6790e
IAmBornedHope <realbeloff@gmail.com>
2026-05-28 00:57:46 +0300

    Stream Output Stream

---
+++ streams/StreamOutputStream.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "../libs/Lab_3/exceptions/Exceptions.hpp"
+#include "OutputStream.hpp"
+#include <memory>
+
+template<typename T>
+class StreamOutputStream: public OutputStream<T> {
+private:
+    std::shared_ptr<OutputStream<T>> stream_;
+public:
+    StreamOutputStream(std::shared_ptr<OutputStream<T>> stream) : stream_{stream} {}
+
+    void write(const T& value) override {
+        if (!stream_) {
+            throw EmptyListException();
+        }
+        stream_->write(value);
+    }
+};
